d39aba400636f5ada034baa5791f9a661c7958e6
Vitosha Labs <opensource@vitosha-labs.bg>
2019-03-28 17:13:40 +0200

    Add initial version of artifact download tool.
    Allows testing of the idea as part of the build process - manual and CI.
    
    There are TODOs around the code which will be implemented if it looks
    OK.

---
+++ bin/artifact_download.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/client'
+require 'optparse'
+require 'yaml'
+
+def parse
+  options = {
+    verify: true,
+    dest_dir: '.',
+  }
+  ARGV << '-h' if ARGV.empty?
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{__FILE__} [options]"
+
+    opts.on("-c FILE", "--configuration=FILE", "Pass configuration file") do |f|
+      options[:config_file] = f
+    end
+
+    opts.on("-d DIR", "--destination=DIR", "Store files in directory") do |d|
+      options[:dest_dir] = d
+    end
+
+    # TODO: pass default
+    opts.on("-v", "--[no-]verify", "Verify hash on downloaded files") do |v|
+      options[:verify] = v
+    end
+
+    opts.on("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  options
+end
+
+def process_config(config)
+  YAML.load_file(config)
+  # TODO: error check
+end
+
+# TODO: check for clashes of files, do hash checks?
+def main
+  opts = parse
+  config = process_config(opts[:config_file])
+  c = ArtifactStorage::Client.new(config: config)
+  c.fetch(dest: opts[:dest_dir])
+end
+
+main
