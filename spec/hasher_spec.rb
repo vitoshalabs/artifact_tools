d107029fe1d01edf3c84f0ba19c17dac033df308
Vitosha Labs <opensource@vitosha-labs.bg>
2019-04-04 18:33:27 +0300

    Add hasher spec.

---
+++ spec/hasher_spec.rb
@@ -0,0 +1,27 @@
+require 'artifact_tools/hasher'
+require 'tempfile'
+
+def sha1sum(path)
+  `sha1sum #{path}`.split[0]
+end
+
+def fill_file_random_data(file)
+  bytes = Random.bytes(Random.rand(1000..10000))
+  file.write(bytes)
+end
+
+class Hash
+  extend ArtifactTools::Hasher
+end
+
+describe ArtifactTools::Hasher do
+  describe 'file_hash' do
+    let(:file) { Tempfile.new('hasher') }
+    before { fill_file_random_data(file) }
+    after { file.unlink }
+
+    it "returns correct hash" do
+      expect(Hash.file_hash(file.path)).to eq sha1sum(file.path)
+    end
+  end
+end
