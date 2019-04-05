e1da9be49087814816bf4fd3221a9c696b8a4805
Vitosha Labs <opensource@vitosha-labs.bg>
2019-04-05 18:02:42 +0300

    Add more user tests.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -17,11 +17,41 @@ end
 describe ArtifactTools::Client do
   let(:fake_ssh) { MockSSH }
   before { stub_const('Net::SSH', fake_ssh) }
+
   describe '.new' do
     it "starts non-interactive session" do
       ArtifactTools::Client.new(config: {})
       expect(MockSSH.start_params[:options]).to have_key(:non_interactive)
       expect(MockSSH.start_params[:options][:non_interactive]).to be
+    end
+
+    it "uses passed user to start connection" do
+      user = 'santa'
+      ArtifactTools::Client.new(config: {}, user: user)
+      expect(MockSSH.start_params[:user]).to eq user
+    end
+
+    context "user in ARTIFACT_STORAGE_USER" do
+      let(:env_user) { 'nick' }
+      before { ENV.update('ARTIFACT_STORAGE_USER' => env_user) }
+
+      it "is used if no user provided as argument" do
+        ArtifactTools::Client.new(config: {})
+        expect(MockSSH.start_params[:user]).to eq env_user
+      end
+
+      it "is overriden by user parameter" do
+        user = 'santa'
+        ArtifactTools::Client.new(config: {}, user: user)
+        expect(MockSSH.start_params[:user]).to eq user
+      end
+
+      it "overrides user in config" do
+        user = 'santa'
+        ArtifactTools::Client.new(config: { 'user' => user })
+        expect(user).not_to eq env_user
+        expect(MockSSH.start_params[:user]).to eq env_user
+      end
     end
   end
 end
