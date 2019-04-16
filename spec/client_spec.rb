c656dbb8bf1d7ed47099b2488dafba0ba6a87841
Vitosha Labs <opensource@vitosha-labs.bg>
2019-04-16 16:55:39 +0300

    Add some more user tests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -31,6 +31,20 @@ describe ArtifactTools::Client do
       expect(MockSSH.start_params[:user]).to eq user
     end
 
+    context 'user in configuration' do
+      let(:arg_user) { 'santa' }
+      let(:config) { { 'user' => 'rudolf' } }
+      it 'is send to server' do
+        ArtifactTools::Client.new(config: config)
+        expect(MockSSH.start_params[:user]).to eq config['user']
+      end
+
+      it 'is overriden by passed argument' do
+        ArtifactTools::Client.new(config: config, user: arg_user)
+        expect(MockSSH.start_params[:user]).to eq arg_user
+      end
+    end
+
     context "user in ARTIFACT_STORAGE_USER" do
       let(:env_user) { 'nick' }
       before { ENV.update('ARTIFACT_STORAGE_USER' => env_user) }
