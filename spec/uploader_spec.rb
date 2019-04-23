839660e989573066b9fff42e629ea01bfc9279ed
Vitosha Labs <opensource@vitosha-labs.bg>
2019-04-23 19:36:36 +0300

    Check that config_file is mandatory option

--- spec/uploader_spec.rb
+++ spec/uploader_spec.rb
@@ -12,12 +12,12 @@ end
 
 describe ArtifactTools::Uploader do
   let(:config_file) { 'artifacts.yaml' }
-  let(:config) { { 'server' => 'xxx', 'dir' => 'yyy', 'files' => {} } }
-  before { stub_const('ArtifactTools::Client', FakeClient) }
-  before { stub_const('ArtifactTools::ConfigFile', FakeConfig) }
-  before { FakeConfig.config = config }
 
   describe '.new' do
+    let(:config) { { 'server' => 'xxx', 'dir' => 'yyy', 'files' => {} } }
+    before { stub_const('ArtifactTools::Client', FakeClient) }
+    before { stub_const('ArtifactTools::ConfigFile', FakeConfig) }
+    before { FakeConfig.config = config }
 
     it "reads config file" do
       ArtifactTools::Uploader.new(config_file: config_file, files:[])
@@ -39,6 +39,22 @@ describe ArtifactTools::Uploader do
         expect(FakeClient.put_files).to eq files
         expect(FakeConfig.object.num_save).to eq 1
       end
+    end
+  end
+
+  describe 'self.parse' do
+    it "requires -c option" do
+      args = %w[-a]
+      expect { ArtifactTools::Uploader.parse(args) }.to raise_error(OptionParser::MissingArgument)
+    end
+
+    it "parses all args" do
+      files = ['file1', 'file2']
+      args = ['-c', config_file, '-a'] + files
+      hash = ArtifactTools::Uploader.parse(args)
+      expect(hash[:config_file]).to eq config_file
+      expect(hash[:append]).to be
+      expect(hash[:files]).to eq files
     end
   end
 end
