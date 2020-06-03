86aa3088274adb34d24bda9a07d5151c50faf9e4
Vitosha Labs <opensource@vitosha-labs.bg>
2020-06-03 17:06:02 +0300

    Uses rubocop fix for offences.

--- lib/artifact_tools/downloader.rb
+++ lib/artifact_tools/downloader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'artifact_tools/client'
 require 'artifact_tools/config_file'
 require 'optparse'
@@ -7,7 +9,6 @@ module ArtifactTools
   # Downloader allows the user to fetch files from a store specified. All this
   # information is provided by {ConfigFile}.
   class Downloader
-
     # Downloads requested files
     #
     # @param config_file [String] Path to configuration file
@@ -15,7 +16,7 @@ module ArtifactTools
     # @param dest_dir [String] Where to download artifacts to
     # @param verify [Boolean] Whether to verify checksums after download.
     # @param match [Regexp] Whether to verify checksums after download.
-    def initialize(config_file:, dest_dir:, user:nil, verify: true, match:nil)
+    def initialize(config_file:, dest_dir:, user: nil, verify: true, match: nil)
       config = load_config(config_file)
       c = ArtifactTools::Client.new(config: config.config, user: user)
       c.fetch(dest: dest_dir, verify: verify, match: match)
@@ -34,33 +35,33 @@ module ArtifactTools
       OptionParser.new do |opts|
         opts.banner = "Usage: #{__FILE__} [options]"
 
-        opts.on("-c FILE", "--configuration=FILE", "Pass configuration file") do |f|
+        opts.on('-c FILE', '--configuration=FILE', 'Pass configuration file') do |f|
           options[:config_file] = f
         end
 
-        opts.on("-d DIR", "--destination=DIR", "Store files in directory") do |d|
+        opts.on('-d DIR', '--destination=DIR', 'Store files in directory') do |d|
           options[:dest_dir] = d
         end
 
-        opts.on("-v", "--[no-]verify", TrueClass, "Verify hash on downloaded files. Default: #{options[:verify]}.") do |v|
+        opts.on('-v', '--[no-]verify', TrueClass, "Verify hash on downloaded files. Default: #{options[:verify]}.") do |v|
           options[:verify] = v
         end
 
-        opts.on("-u USER", "--user=USER", "Access server with this username") do |u|
+        opts.on('-u USER', '--user=USER', 'Access server with this username') do |u|
           options[:user] = u
         end
 
-        opts.on("-m REGEXP", "--match=REGEXP", Regexp, "Download only file which match regular expression") do |v|
+        opts.on('-m REGEXP', '--match=REGEXP', Regexp, 'Download only file which match regular expression') do |v|
           options[:match] = v
         end
 
-        opts.on("-h", "--help", "Show this message") do
+        opts.on('-h', '--help', 'Show this message') do
           puts opts
           exit
         end
       end.parse!(arguments)
 
-      raise OptionParser::MissingArgument.new("Missing -c/--configuration option") unless options.has_key?(:config_file)
+      raise OptionParser::MissingArgument, 'Missing -c/--configuration option' unless options.key?(:config_file)
 
       options
     end
